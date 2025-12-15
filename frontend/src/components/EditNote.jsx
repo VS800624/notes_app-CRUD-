2390ea9e38dbb1e4a2fd495b2d3fa7ad68be3a22
VS800624 <vishalsingh800624@gmail.com>
2025-12-15 20:54:17 +0530

    Created Edit Note page

--- frontend/src/components/EditNote.jsx
+++ frontend/src/components/EditNote.jsx
@@ -1,31 +1,53 @@
 import React from "react";
 import { useState } from "react";
 import { BASE_URL } from "../utils/constants";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
+import { useEffect } from "react";
 
-const CreateNote = () => {
+const EditNote = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [error, setError] = useState("");
+  const [showToast, setShowToast] = useState(false)
+  const {id} = useParams()
   const navigate = useNavigate()
 
-  const handleCreate = async () => {
+  useEffect(() => {
+    const fetchNote = async () => {
+      try{
+        const res = await axios.get(`${BASE_URL}/note/${id}`, 
+        {withCredentials: true}
+      )
+      setTitle(res.data.data.title)
+      setDescription(res.data.data.description)
+      }catch(err){
+      setError("Failed to load note");
+      }
+    }
+    fetchNote()
+  }, [id])
+
+  const handleEdit = async () => {
     if (!title || !description) {
       setError("All fields are required");
       return
     }
     try {
-      const res = await axios.post(
-        BASE_URL + "/notes/create",
+      const res = await axios.put(
+        BASE_URL + "/notes/edit/" +id,
         {
           title,
           description,
         },
         { withCredentials: true }
       );
-      navigate("/")
+      
       setError("")
+      setShowToast(true)
+      setTimeout(() => {
+        setShowToast(false)
+      },3000)
     } catch (err) {
       setError(err?.response?.data?.message || "Something went wrong");
       console.log(err);
@@ -33,14 +55,21 @@ const CreateNote = () => {
   };
 
   return (
-    <div className="min-h-screen flex justify-center items-start">
-      <div className="w-full max-w-xl bg-white mt-20 p-6 rounded-xl shadow-md text-gray-800">
+    <>
+    <div className="min-h-screen flex justify-center items-start  mt-10">
+      <button
+        className="bg-blue-600 text-white font-semibold py-2 px-6 rounded-lg hover:bg-blue-800 transition"
+        onClick={() => navigate(-1)}
+      >
+        Back
+      </button>
+      <div className="w-full max-w-xl bg-gray-200 mt-20 text-gray-800 p-6 rounded-xl shadow-md">
         <h1 className="text-center text-2xl font-bold mb-6 ">
-          Create Note
+          Edit Note
         </h1>
 
         {/* Title */}
-        <div className="mb-4 ">
+        <div className="mb-4">
           <label className="block font-semibold text-gray-700 mb-2">
             Title
           </label>
@@ -74,13 +103,19 @@ const CreateNote = () => {
         {/* Button */}
         <button
           className="w-full bg-green-600 text-white font-semibold py-2 rounded-lg hover:bg-green-800 transition"
-          onClick={handleCreate}
+          onClick={handleEdit}
         >
           Save
         </button>
       </div>
     </div>
+      {showToast && (<div className="toast toast-top toast-center">
+        <div className="alert alert-success">
+          <span>Profile saved successfully.</span>
+        </div>
+      </div>)}
+    </>
   );
 };
 
-export default CreateNote;
+export default EditNote;
