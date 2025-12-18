aac637352a1b2ba8fc0f7d1ed52c5cfab9b2217e
VS800624 <vishalsingh800624@gmail.com>
2025-12-18 16:45:27 +0530

    add password and email in note schema

--- backend/src/models/note.js
+++ backend/src/models/note.js
@@ -1,4 +1,6 @@
 const { default: mongoose } = require("mongoose");
+const validator = require('validator')
+
 
 const noteSchema = new mongoose.Schema(
   {
@@ -13,6 +15,28 @@ const noteSchema = new mongoose.Schema(
       required: true,
       unique: true
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowerCase: true,
+      trim: true,
+      validate(value){
+        if(!validator.isEmail(value)){
+          throw new Error("Invalid Email Address:" + value)
+        }
+      }
+    },
+    password: {
+    type: String,
+    required: true,
+    minlength: 6,
+     validate(value) {
+      if(!validator.isStrongPassword(value)){
+        throw new Error("Password must be strong (min 8 chars, uppercase, lowercase, number, special char)")
+      }
+    }
+  },
   },
   { timestamp: true }
 );
