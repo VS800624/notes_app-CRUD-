241e07ec1f081aa1f05bad2cecbe5e551cd6b2d7
VS800624 <vishalsingh800624@gmail.com>
2025-12-26 18:18:25 +0530

    Add payment schema for Razorpay

---
+++ backend/src/models/payment.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+
+
+const paymentSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: User,     // attaching this collection to the user collection i.e. each payment should be linked to a user
+    required: true
+  },
+  paymentId: {
+    type: String,
+  },
+  orderId: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  },
+    amount: {
+      type: Number,
+      required: true
+    },
+    currency: {
+      type: String,
+      required: true,
+    },
+    receipt: {
+      type: String,
+      required: true
+    },
+    notes: {
+      firstName: {
+        type: String
+      },
+      lastName: {
+        type: String
+      },
+    }
+
+}, {timestamps: true})
+
+module.exports = new mongoose.model("payment", paymentSchema)
